dba00f7730d8e154778c32347ff944f7eaa80328
Michael Mazour <michaelmazour@gmail.com>
2019-03-24 10:07:37 +0000

    Add Coolpay
    Bundle in mmazour's Coolpay gem.

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in fakepay.gemspec
 gemspec
+
+gem 'coolpay', path: '../mmazour_coolpay'
--- spec/fakepay_spec.rb
+++ spec/fakepay_spec.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+require 'coolpay'
+
 RSpec.describe Fakepay do
   it 'has a version number' do
     expect(Fakepay::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'loads Coolpay' do
+    expect(Coolpay).to be
+    expect(Coolpay::Recipient).to be
+    expect(Coolpay::Payment).to be
   end
 end
